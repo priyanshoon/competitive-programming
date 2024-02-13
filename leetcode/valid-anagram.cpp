d81c1cc9cc1be4a138ed34b5b53d517ef3e3a6f1
priyanshoon <62681950+priyanshoon@users.noreply.github.com>
2024-02-13 22:03:47 +0530

    valid anagram

--- leetcode/valid-anagram.cpp
+++ leetcode/valid-anagram.cpp
@@ -2,10 +2,22 @@
 using namespace std;
 
 bool isAnagram(string s, string t) {
-
+    if (s.length() != t.length()) return false;
+    int n = s.length();
+    unordered_map<char, int> m;
+    for (int i = 0; i < n; i++) {
+        m[s[i]]++;
+        m[t[i]]--;
+    }
+    for (auto it : m) {
+        if (it.second != 0) return false;
+    }
+    return true;
 }
 
 int main (int argc, char *argv[]) {
-
+    string s = "anagram";
+    string t = "nagaram";
+    cout << isAnagram(s, t) << endl;
     return 0;
 }
